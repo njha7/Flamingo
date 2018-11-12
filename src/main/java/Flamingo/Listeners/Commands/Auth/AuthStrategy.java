775ad1d57ed0de6d59ff8e4371991bf6802de339
Neel Jha <neel.jha@gatech.edu>
2018-11-12 17:03:02 -0500

    Strike + strike clear command work. TODO: DI

--- src/main/java/Flamingo/Listeners/Commands/Auth/AuthStrategy.java
+++
@@ -1,8 +0,0 @@
-package Flamingo.Listeners.Commands.Auth;
-
-import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
-
-public interface AuthStrategy {
-
-    public boolean isAuthorized(MessageReceivedEvent event);
-}
